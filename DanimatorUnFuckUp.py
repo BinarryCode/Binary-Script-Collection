21330950faceec7a0a639c0786ad2d0f8d91604b
BinarryCode <53833277+BinarryCode@users.noreply.github.com>
2022-01-29 15:48:44 -0500

    Create DanimatorUnFuckUp.py

---
+++ DanimatorUnFuckUp.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+print("Danimator Un-Fuck-Up Script")
+print("Made by Binrary#6656")
+print("Because the developer didn't open-source it.")
+print("To use, edit the bottom of this file to contain offsets formatted as so:")
+print("Offset(X, Y, SpriteX, SpriteY)")
+print("X and Y should be the same as it is in Danimator.")
+print("SpriteX and SpriteY should be the same as they are in SLADE3.")
+
+def Offset(movedX, movedY, spriteX, spriteY):
+	finalX = (-movedX + -spriteX)
+	finalY = (-movedY + -spriteY)
+	print("Offset(" + str(finalX) + ", " + str(finalY) + ")")
+
+# Put Offset lines below here
